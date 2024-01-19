f7a58e0696aa9e8baa9c12ef60cee826b6e35a04
JyojiTenguam <j.y.oji@hotmail.com>
2024-01-18 21:36:59 -0300

    Resolvendo requisito 01 de Exception handling

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-function calculateSum() {
-  const value1 = document.getElementById('value1').value;
-  const value2 = document.getElementById('value2').value;
-  const result = Number(value1) + Number(value2);
+const ValueNumber = (number1, number2) => {
+  if (typeof number1 !== '' || typeof number2 !== '')
+    throw new Error("Preencha os campos para realizar a soma")
+}
 
-  return result;
+function calculateSum() {
+  try {
+    const value1 = document.getElementById('value1').value;
+    const value2 = document.getElementById('value2').value;
+    ValueNumber(value1, value2);
+    const result = Number(value1) + Number(value2);
+    return result;
+  } catch (error) {
+  return error.message;
+  }
 }
 
 function displayResult(result) {
